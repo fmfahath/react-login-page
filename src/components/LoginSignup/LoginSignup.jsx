59da475f745bf72668e61ec7eae2dabcf2eb05f1
fmfahath <fmfahath.dev@gmail.com>
2024-01-17 19:11:22 +0530

    folder structure created+

---
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './LoginSignup.css';
+
+import user_icon from '../assets/person.png';
+import email_icon from '../assets/email.png';
+import password_icon from '../assets/password.png';
+
+const LoginSignup = () => {
+  return (
+    <div>
+        <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default LoginSignup
