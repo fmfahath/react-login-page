e944d07632879b935a72c6e3f93869811d8810b0
fmfahath <fmfahath.dev@gmail.com>
2024-01-17 19:31:47 +0530

    html layout created

--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -7,8 +7,32 @@ import password_icon from '../assets/password.png';
 
 const LoginSignup = () => {
   return (
-    <div>
-        <h1>Hello</h1>
+    <div className='container'>
+        <div className='header'>
+            <div className='text'>Sign Up</div>
+            <div className='underline'></div>
+        </div>
+        <div className='inputs'>
+            <div className='input'>
+                <img src={user_icon}/>
+                <input type='text'/>
+            </div>
+            <div className='input'>
+                <img src={email_icon}/>
+                <input type='email'/>
+            </div>
+            <div className='input'>
+                <img src={password_icon}/>
+                <input type='password'/>
+            </div>
+        </div>
+        <div className='forgot-password'>
+            Lost password? <span>Click Here</span>
+        </div>
+        <div className='submit-container'>
+            <div className='submit'>Sign Up</div>
+            <div className='submit'>Login</div>
+        </div>
     </div>
   )
 }
