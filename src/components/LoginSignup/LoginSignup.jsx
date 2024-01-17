e7d9ac09e95e3943ff494bbd79ada84154a7afba
fmfahath <fmfahath.dev@gmail.com>
2024-01-17 21:33:33 +0530

    forgot password & name field show/hide condition added

--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -12,35 +12,47 @@ const LoginSignup = () => {
 
 
 
-  return (
-    <div className='container'>
-        <div className='header'>
-            <div className='text'>{action}</div>
-            <div className='underline'></div>
-        </div>
-        <div className='inputs'>
-            <div className='input'>
-                <img src={user_icon}/>
-                <input type='text' placeholder='Name'/>
+    return (
+        <div className='container'>
+            <div className='header'>
+                <div className='text'>{action}</div>
+                <div className='underline'></div>
             </div>
-            <div className='input'>
-                <img src={email_icon}/>
-                <input type='email'  placeholder='Email'/>
+            <div className='inputs'>
+
+                {/* show/hide name field */}
+                {action === "Login" ?
+                    <div></div> :
+                    <div className='input'>
+                        <img src={user_icon} />
+                        <input type='text' placeholder='Name' />
+                    </div>
+                }
+
+                <div className='input'>
+                    <img src={email_icon} />
+                    <input type='email' placeholder='Email' />
+                </div>
+                <div className='input'>
+                    <img src={password_icon} />
+                    <input type='password' placeholder='Password' />
+                </div>
             </div>
-            <div className='input'>
-                <img src={password_icon}/>
-                <input type='password'  placeholder='Password'/>
+
+            {/* show/Hide forgot passord text */}
+            {action === "Sign Up" ?
+                <div></div> :
+                <div className='forgot-password'>
+                    Lost password? <span>Click Here</span>
+                </div>
+            }
+
+            <div className='submit-container'>
+                <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { setAction("Sign Up") }}>Sign Up</div>
+                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
             </div>
         </div>
-        <div className='forgot-password'>
-            Lost password? <span>Click Here</span>
-        </div>
-        <div className='submit-container'>
-            <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => {setAction("Sign Up")}}>Sign Up</div>
-            <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => {setAction("Login")}}>Login</div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default LoginSignup
