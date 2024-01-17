c7cfb9cb7509a6b74a776cc5b7d431eebacbf963
fmfahath <fmfahath.dev@gmail.com>
2024-01-17 21:23:20 +0530

    useState created

--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginSignup.css';
 
 import user_icon from '../assets/person.png';
 import email_icon from '../assets/email.png';
 import password_icon from '../assets/password.png';
+import { act } from 'react-dom/test-utils';
 
 const LoginSignup = () => {
+
+    const [action, setAction] = useState("Sign Up");
+
+
+
   return (
     <div className='container'>
         <div className='header'>
-            <div className='text'>Sign Up</div>
+            <div className='text'>{action}</div>
             <div className='underline'></div>
         </div>
         <div className='inputs'>
@@ -30,8 +36,8 @@ const LoginSignup = () => {
             Lost password? <span>Click Here</span>
         </div>
         <div className='submit-container'>
-            <div className='submit'>Sign Up</div>
-            <div className='submit'>Login</div>
+            <div className={action === "Login" ? "submit gray" : "submit"}>Sign Up</div>
+            <div className={action === "Sign Up" ? "submit gray" : "submit"}>Login</div>
         </div>
     </div>
   )
